5f44398c5fbb5dd39ea76da2cbf98ad4769ee717
Muhammad Ziada <engziada@gmail.com>
2023-09-03 00:14:03 +0300

    Create admin.py

---
+++ pages/admin.py
@@ -0,0 +1,7 @@
+# login.py (inside the 'pages' directory)
+import streamlit as st
+
+def show():
+    st.title("Admin Page")
+    
+    # Add your Streamlit components and logic for the login page here
